4167a2800f4637dd68f9e1e52595eb128ce05447
animsh <animsh.more@gmail.com>
2021-05-31 23:27:06 +0530

    changelog: added dob calculator in minutes

---
+++ DOBCalculator/app/src/main/java/com/animsh/dobcalculator/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.animsh.dobcalculator
+
+import android.app.DatePickerDialog
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
+import java.text.SimpleDateFormat
+import java.util.*
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+        btnDatePicker.setOnClickListener { view ->
+            clickDataPicker(view)
+        }
+    }
+
+    private fun clickDataPicker(view: View) {
+        val c = Calendar.getInstance()
+        val year =
+                c.get(Calendar.YEAR)
+        val month = c.get(Calendar.MONTH)
+        val day = c.get(Calendar.DAY_OF_MONTH)
+
+        val dpd = DatePickerDialog(
+                this,
+                DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
+                    val selectedDate = "$dayOfMonth/${monthOfYear + 1}/$year"
+
+                    tvSelectedDate.text = selectedDate
+                    val sdf = SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH)
+                    val theDate = sdf.parse(selectedDate)
+                    val selectedDateToMinutes = theDate!!.time / 60000
+                    val currentDate = sdf.parse(sdf.format(System.currentTimeMillis()))
+                    val currentDateToMinutes = currentDate!!.time / 60000
+
+                    val differenceInMinutes = currentDateToMinutes - selectedDateToMinutes
+                    tvSelectedDateInMinutes.text = differenceInMinutes.toString()
+                },
+                year,
+                month,
+                day
+        )
+        dpd.datePicker.maxDate = Date().time - 86400000
+        dpd.show()
+    }
+}
